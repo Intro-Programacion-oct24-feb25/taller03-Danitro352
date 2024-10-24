fbf88ef04a686372a0678871434ab7bbb6d18124
Daniel <doidrovo@utpl.edu.ec>
2024-10-24 12:36:40 -0500

    ejercicio4

---
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int var1 = 20;
+       int var2 = 1;
+       int var3 = 3;
+       int var4 = 2;
+       double division = (var1/(var2+var3));
+       double suma = (division + var4);
+       boolean resultado = (suma >= var3);
+       System.out.println(resultado);
+             
+    }
+    
+}
